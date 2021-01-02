8f27277de4116d6898da380f1f0ac4bcb7f6575d
ahmedshaikh123 <ahmedshaikh78699@gmail.com>
2021-01-02 17:05:39 +0530

    commit

--- push.cmd
+++ push.cmd
@@ -5,17 +5,17 @@
 TITLE *****CREATED BY AHMED SHAIKH*****
 
 ECHO Adding File please wait... 
-echo
+
 git add .
 echo "Enter your changes info: " 
-echo 
+ 
 read commit   
-echo
+
 echo "Your changes info is as  $commit "  
-echo  
+ 
 git commit -m commit
-echo
+
 ECHO Commiting please wait... 
-echo
+
 git push
 ECHO Push Done
