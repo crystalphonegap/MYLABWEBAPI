a416b705a88e12430f74fe1dd2e2213602a9c1dc
ahmedshaikh123 <ahmedshaikh78699@gmail.com>
2021-01-02 17:12:20 +0530

    ''

--- push.cmd
+++ push.cmd
@@ -9,9 +9,9 @@ ECHO Adding File please wait...
 git add .
 set /p name= Enter your changes info: ? 
 
-echo "Your changes info is as "  $name
+echo "Your changes info is as "  '%choice%'
  
-git commit -m $name
+git commit -m '%choice%'
 
 ECHO Commiting please wait... 
 
